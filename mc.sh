993acba94c07d36618f16e33b7817c108f9e6096
Kasper Nielsen <kasperni@gmail.com>
2014-07-14 10:25:10 +0200

    update pom.xml

--- mc.sh
+++ mc.sh
@@ -2,6 +2,7 @@
 #Any command that fails will exit the script
 set -e 
 
+#####Make sure we do not have uncommitted changes
 git status
 if ! git diff-index --quiet HEAD --; then
   echo "There are uncommitted changes to /MaritimeCloud"
@@ -18,23 +19,28 @@ echo "Release version: $release"
 echo "Next version: $next"
 read -p "Press enter to start release process: " enter
 
+######Test build, lets make sure it builds
 mvn -DperformRelease=true clean source:jar javadoc:jar install
 
-#Lets bump the version counter
+######Lets bump the version counter
 
 mvn versions:set -DnewVersion=$release
 mvn -DperformRelease=true clean source:jar javadoc:jar install deploy
 mvn versions:commit
+
+###### Push changes to git
 
 git add '*pom.xml'
 
 git commit -m "Releasing $release  [ci skip]"
 git push
 
+###### Create a tag in git
 git tag -a v$release -m 'Version $release'
 git push origin v$release
 
 
+##### Preparing next development version
 mvn versions:set -DnewVersion=$next
 mvn clean source:jar javadoc:jar install deploy
 mvn versions:commit
