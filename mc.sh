b2c8193d9e2468688947b273b2db21212b073c07
Kasper Nielsen <kasperni@gmail.com>
2014-07-14 09:56:32 +0200

    update

---
+++ mc.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#Any command that fails will exit the script
+set -e 
+
+git status
+if ! git diff-index --quiet HEAD --; then
+  echo "There are uncommitted changes to /MaritimeCloud"
+  exit 1
+fi
+
+
+read -p "Enter release version: " release
+read -p "Enter Next release version (without snapshot): " next
+
+next="$next-SNAPSHOT"
+echo ""
+echo "Release version: $release"
+echo "Next version: $next"
+read -p "Press enter to start release process: " enter
+
+mvn -DperformRelease=true clean source:jar javadoc:jar install
+
+//Lets bump the version counter
+
+mvn versions:set -DnewVersion=$release
+mvn -DperformRelease=true clean source:jar javadoc:jar install
+mvn versions:commit
