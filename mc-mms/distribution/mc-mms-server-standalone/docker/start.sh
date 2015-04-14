9d023c08e54c9dc6f8993a2bdb189fa9aee79725
peder280370 <peder@carolus.dk>
2015-04-14 09:07:48 +0200

    Fix docker ports

--- mc-mms/distribution/mc-mms-server-standalone/docker/start.sh
+++ mc-mms/distribution/mc-mms-server-standalone/docker/start.sh
@@ -1,5 +1,5 @@
 
 LATEST=`ls /archive/mc-mms/distribution/mc-mms-server-standalone/target/mc-mms-server-standalone-*SNAPSHOT.jar`
 
-java -classpath $LATEST net.maritimecloud.mms.server.Main -port 9090
+java -classpath $LATEST net.maritimecloud.mms.server.Main -port 43234 -rest 9090
 
