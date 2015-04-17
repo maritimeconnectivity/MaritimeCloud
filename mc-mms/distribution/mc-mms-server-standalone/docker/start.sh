d636d429bb761fd47b6be02cffad58ec95d5da2f
peder280370 <peder@carolus.dk>
2015-04-17 15:32:34 +0200

    Support binary wire protocol for docker MMS server

--- mc-mms/distribution/mc-mms-server-standalone/docker/start.sh
+++ mc-mms/distribution/mc-mms-server-standalone/docker/start.sh
@@ -1,5 +1,12 @@
 
+
+if [ -z "${BINARY}" ]; then
+	BINARYP=""
+else
+	BINARYP="-Dnet.maritimecloud.mms.use_binary=${BINARY}"
+fi
+
 LATEST=`ls /archive/mc-mms/distribution/mc-mms-server-standalone/target/mc-mms-server-standalone-*SNAPSHOT.jar`
 
-java -classpath $LATEST net.maritimecloud.mms.server.Main -port 43234 -rest 9090
+java -classpath $LATEST net.maritimecloud.mms.server.Main -port 43234 -rest 9090 BINARYP
 
